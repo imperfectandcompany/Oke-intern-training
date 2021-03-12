d13d129ce42527e5693c8cce2c67eff3996a82f7
Oke Amuwha <gv4281@wayne.edu>
2021-03-12 16:28:39 -0500

    Replaced reference variables and for statement with dynamic switches
    Learned how to use switch statements to replace the header file name on a website dynamically

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php
 include_once("functions.php"); // Calls file that contains the funcfions
 // Function to call header
-head("Home");
+head();
 
 include_once("./public/sections/body.php"); // Include path file to access folders
 // ./ starts from the same directory it's in
