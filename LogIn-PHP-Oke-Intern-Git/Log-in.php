2a5a6629a7e2215f4f22f9511de90a91c23c9515
Oke Amuwha <amuwhao46@gmail.com>
2021-03-26 16:35:45 -0400

    Notes for atom design

--- LogIn-PHP-Oke-Intern-Git/Log-in.php
+++ LogIn-PHP-Oke-Intern-Git/Log-in.php
@@ -1,15 +1,16 @@
 <html>
 <head>
     <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
     <title>Login - Postogon</title>
     <link rel="" href=""></link>
 </head>
 <body>
     <h1> Login</h1>
     <p>Log in with username and password</p>
+    <!--Lets server choose what type of information will be processed through-->
     <form>
     <input></input>
     <button></button>
