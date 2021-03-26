a1f5b36de2eeec17c3fcad094e24b9432cb53637
Oke Amuwha <amuwhao46@gmail.com>
2021-03-26 16:21:04 -0400

    added boiler plate

--- LogIn-PHP-Oke-Intern-Git/Log-in.php
+++ LogIn-PHP-Oke-Intern-Git/Log-in.php
@@ -1,10 +1,14 @@
 <html>
 <head>
-<title></title>
-<link rel="" href=""></link>
+    <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
+    <title>Login - Postogon</title>
+    <link rel="" href=""></link>
 </head>
 <body>
-<h1> Header</h1>
-<p>Test</p>
+    <h1> Login</h1>
+    <p>Log in with username and password</p>
 </body>
 </html>
